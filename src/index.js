9af2d54a64c1c51fb7912dc5d63bb76ac6aa8ca0
Vladislav <bakalavrat.789@gmail.com>
2023-02-10 14:34:23 +0200

    Update tml

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
+import coutryCardTpl from "./templates/cards-country.hbs";
 
 const DEBOUNCE_DELAY = 300;
+fetch('https://restcountries.com/v2/all')
+  .then(response => {
+    return response.json('');
+  })
+
+  .then(all => {
+    console.log(capital);
+    const markup = coutryCardTpl(all);
+    console.log(markup)
+  })
+  .catch(error => {
+    console.log(error);
+  });
